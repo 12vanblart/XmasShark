78e1f3be01d1aeafea740b607352986b2d80326e
Tyler <12vanblart@gmail.com>
2018-10-01 11:29:20 +0000

    Background Story work

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,37 @@
 import React from "react";
 import Layout from "../components/layout";
+import BlockQuote from "../components/blockquote";
 
 export default () => (
 	<Layout>
-		<em>Oops! This page isn't ready yet! Check back later!</em>
+		<h1>The Background Story</h1>
+		<p>
+			<em>Twas the night before Christmas... </em>
+			<b>Not really.</b>
+		</p>
+		<p>
+			It was just your average family Christmas cookie baking session when
+			we heard our mom ask,
+		</p>
+		<BlockQuote>
+			"<em>What</em> are these?"
+		</BlockQuote>
+		<p>
+			She had just walked into the room where we were cutting out the
+			cookies from the oh-so-delicious homemade cookie dough... and my
+			brother had taken a shark cookie cutter to one of the last
+			doughballs.
+		</p>
+		<BlockQuote>
+			"<em>It's the Christmas Shark!</em>" my brother responded
+			matter-of-factly.
+		</BlockQuote>
+		<p>
+			<b>And thus the Shark was born.</b>
+		</p>
+		<p>
+			Ever since, it's been a recurring family joke. But also...{" "}
+			<em>how else would Santa deliver presents to Atlantis</em>
+		</p>
 	</Layout>
 );
