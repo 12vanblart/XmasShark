30f8816fe63e9ecef8fdcd1148667fd24e7f8219
Tyler <12vanblart@gmail.com>
2018-10-04 11:28:13 +0000

    Adding logos to merch page

--- src/pages/merch.js
+++ src/pages/merch.js
@@ -5,6 +5,31 @@ import mom from "./Retina_Short_Mama.png";
 import baby from "./Retina_Short_Baby.png";
 import shark from "./Retina_Short_Shark.png";
 import mc from "./Retina_Short_MC.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAmazon } from "@fortawesome/free-brands-svg-icons";
+import rbLogo from "./rbubbleLogo.jpg";
+
+const Amazon = props => (
+	<a
+		href={props.to}
+		style={{
+			backgroundImage: `none`,
+		}}
+	>
+		<FontAwesomeIcon icon={faAmazon} />
+	</a>
+);
+
+const RedBubble = props => (
+	<a
+		href={props.to}
+		style={{
+			backgroundImage: `none`,
+		}}
+	>
+		<img icon={faAmazon} src={rbLogo} />
+	</a>
+);
 
 export default () => (
 	<Layout>
@@ -15,8 +40,10 @@ export default () => (
 		</div>
 		<hr />
 		<BlockQuote>
-			Santa Shark
+			<h1>Santa Shark</h1>
 			<img src={shark} />
+			<Amazon to="" />
+			<RedBubble to="" />
 		</BlockQuote>
 	</Layout>
 );
